90f290a16313540d2ca468bf45c906f7ad44b0c6
AlexDeni <alexdenisenko94@gmail.com>
2020-04-06 02:34:34 +0300

    book-design

---
+++ src/ui/Logo/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from '../../static/logo.png';
+
+function Logo() {
+    return(
+        <img className='logo' src={logo} alt="logo" />
+    )
+}
+
+export {Logo}
