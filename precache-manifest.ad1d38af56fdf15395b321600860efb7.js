d21876b836d2a4ba599f78d4e67e0b6ff3f01a4d
AlexDeni <alexdenisenko94@gmail.com>
2020-05-21 22:08:46 +0300

    Updates

---
+++ precache-manifest.ad1d38af56fdf15395b321600860efb7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7c088358d629c7ae67c65f413342f1",
+    "url": "/Books/index.html"
+  },
+  {
+    "revision": "556715642f4edbe9f255",
+    "url": "/Books/static/css/main.bcb3c3fb.chunk.css"
+  },
+  {
+    "revision": "f8667a6ede3ef96c279b",
+    "url": "/Books/static/js/2.9059974b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Books/static/js/2.9059974b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "556715642f4edbe9f255",
+    "url": "/Books/static/js/main.edfc9b4c.chunk.js"
+  },
+  {
+    "revision": "716a82c1da4adc3ef042",
+    "url": "/Books/static/js/runtime-main.29aebfe8.js"
+  },
+  {
+    "revision": "39305e8b476f9045a32c5874b1ecec93",
+    "url": "/Books/static/media/logo.39305e8b.png"
+  },
+  {
+    "revision": "ff2adb60fa04164963c0cea0c1b194ae",
+    "url": "/Books/static/media/montserratbold.ff2adb60.ttf"
+  },
+  {
+    "revision": "8ce89b9e3b1d099510aaebb11556a09e",
+    "url": "/Books/static/media/slide2.8ce89b9e.jpg"
+  },
+  {
+    "revision": "13b28c484fde1b2fdd25288cbfc70a15",
+    "url": "/Books/static/media/slide3.13b28c48.jpg"
+  },
+  {
+    "revision": "835a2d4aa3a477125a97d677b03e163c",
+    "url": "/Books/static/media/slide4.835a2d4a.jpg"
+  },
+  {
+    "revision": "5242f54cfba89be617792728b4188a17",
+    "url": "/Books/static/media/slide5.5242f54c.jpg"
+  },
+  {
+    "revision": "5513e6aa4c4c23f02002a156e84b9a04",
+    "url": "/Books/static/media/slide6.5513e6aa.jpg"
+  }
+]);
